66c33a32f126737f2af7e047df22abe5afbf0c79
mohammedAlhammouri <mohammedAlhammouri98@gmail.com>
2017-11-22 10:20:56 +0200

    Haneen first push

--- src/com/example/usecase/CreateStudent2.java
+++ src/com/example/usecase/CreateStudent2.java
@@ -10,7 +10,7 @@ public class CreateStudent2 implements UseCase<HashMap<String,String>,Student>{
 
     @Override
     public Student execute(HashMap<String, String> hashMap) throws Exception {
-///////////////
+//////////////////
         return null;
     }
 
