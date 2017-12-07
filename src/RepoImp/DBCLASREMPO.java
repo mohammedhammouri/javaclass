148cafc468be15be5d1ac09998a1a160c861fa41
haneenabdeen <haneenabdin110@gmail.com>
2017-12-06 20:05:33 -0800

    //////CoppyJavaClass

---
+++ src/RepoImp/DBCLASREMPO.java
@@ -0,0 +1,62 @@
+package RepoImp;
+
+import com.company.clas;
+import repo.ClassRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBCLASREMPO implements ClassRepo {
+    List<clas> classes = new ArrayList<>();
+    @Override
+    public clas create(clas clas) {
+        classes.add(clas);
+        return clas;
+
+    }
+
+    @Override
+    public clas update(String clasname, clas clas) {
+        for (int i = 0; i < classes.size(); i++) {
+            if (classes.get(i).getClassName().equals(clasname))
+                classes.set(i,clas);
+            return clas;
+        }
+        return clas;  }
+
+    @Override
+    public clas remove(clas clas) {
+        classes.remove(clas);
+        return clas;
+
+    }
+
+    @Override
+    public clas get(String clasname,clas clas) {
+        for (int i = 0; i < classes.size(); i++) {
+            if( classes.get(i).getClassName().equals(clasname))
+
+                return clas;
+
+        }
+        return null;
+    }
+
+    @Override
+    public List<clas> getall() {
+        return classes;
+    }
+
+    @Override
+    public List<clas> quary(String clasname, String teacherName) {
+        //select * from clas where classname=""and teacherName=""
+
+        for (int i = 0; i < classes.size(); i++) {
+            if(classes.get(i).getClassName().equals(clasname)
+                    ||classes.get(i).getTeacherName().equals(teacherName))
+
+
+
+        }
+
+    }
--- src/factory/classRepoFactory.java
+++ src/factory/classRepoFactory.java
@@ -1,12 +1,14 @@
 package factory;
 
+import RepoImp.DBCLASREMPO;
+import RepoImp.INMERepo;
 import com.company.clas;
 import repo.ClassRepo;
 
 import java.util.List;
 
 public class classRepoFactory implements Factory {
-    ClassRepo CLASREPO ;
+   private ClassRepo CLASREPO ;
     private boolean test ;
 
     public void setTest(boolean test) {
@@ -17,8 +19,13 @@ public class classRepoFactory implements Factory {
     public ClassRepo getCLASREPO() {
 
         if(getCLASREPO()==null)
-            if (test)
-               ClassRepo getCLASREPO=new
+            if (test){
+                 ClassRepo = new INMERepo();}
+                else {
+               ClassRepo  = new DBCLASREMPO();
+            }
+
+
 
 
 
